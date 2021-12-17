34a83258750658edd78e5f0ebb0736b7b4b80b29
AV.Pavlov <AV.Pavlov@5-311-12>
2021-12-17 15:50:15 +0300

    Time

--- Doctor/DocSelection.xaml.cs
+++ Doctor/DocSelection.xaml.cs
@@ -22,10 +22,11 @@ namespace Doctor
     /// </summary>
     public partial class DocSelection : Window
     {
-        DataTable dt1 = new DataTable("Time");
+        DataTable dt1 = new DataTable();
         public DocSelection()
         {
             InitializeComponent();
+            dt1.Columns.Add("Time", typeof(String));
         }
 
         private void DpDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -36,21 +37,38 @@ namespace Doctor
                 using (SQLiteConnection connection = new SQLiteConnection(DBConnection.myConn))
                 {
                     connection.Open();
-                    var Date_in = DpDate.SelectedDate;
+                    var Date_in = DpDate.Text;
                     var Doc_in = CbDoc.SelectedItem;
-                    string query = $@"SELECT ScheduleStart FROM DocSchedule WHERE DocID = {Doc_in}  AND Day = {Date_in}";
-                    string query1 = $@"SELECT ScheduleEND FROM DocSchedule WHERE DocID = {Doc_in}  AND Day = {Date_in}";
+                    string query = $@"SELECT ScheduleStart FROM DocSchedule WHERE DocID = '2'  AND Day = '{Date_in}'";
+                    string query1 = $@"SELECT ScheduleEND FROM DocSchedule WHERE DocID = '2'  AND Day = '{Date_in}'";
                     SQLiteCommand cmd = new SQLiteCommand(query, connection);
                     SQLiteCommand cmd1 = new SQLiteCommand(query1, connection);
-                    string start = Convert.ToString(cmd.ExecuteReader());
-                    string end = Convert.ToString(cmd1.ExecuteReader());
+                    string start = Convert.ToString(cmd.ExecuteScalar());
+                    string end = Convert.ToString(cmd1.ExecuteScalar());
                     DateTime schstart = DateTime.Parse(start);
                     DateTime schend = DateTime.Parse(end);
-
-                    for (DateTime ii11 = schstart; ii11 < schend; schstart.AddMinutes(15))
+                    for (DateTime ii11 = schstart; ii11 < schend; ii11 = ii11.AddHours(0.25))
                     {
-                        dt1.Rows.Add(ii11);
+                        DataRow _ravi = dt1.NewRow();
+                        //MessageBox.Show(ii11.ToString("t"));
+                        _ravi["Time"] = ii11.ToString("t");
+                        dt1.Rows.Add(_ravi);
                     }
+                    string query2 = $@"SELECT TimeApp FROM Appointment WHERE DocID = '2'  AND DateApp = '{Date_in}'";
+                    SQLiteCommand cmd2 = new SQLiteCommand(query2, connection);
+                    DataTable dt2 = new DataTable();
+                    dt2.Load(cmd2.ExecuteReader());
+                    foreach(DataRow ttt in dt2.Rows)
+                    {
+                        string time = Convert.ToString(ttt["TimeApp"]);
+                        for (int i = dt1.Rows.Count - 1; i >= 0; i--)
+                        {
+                            DataRow dr = dt1.Rows[i];
+                            if (dr["Time"].ToString() == time)
+                                dr.Delete();
+                        }
+                    }
+                    CbTime.ItemsSource = dt1.DefaultView;
                 }
             }
             catch (SQLiteException ex)
