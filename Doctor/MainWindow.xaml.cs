3f521a66227b4ad2a6814a7fac32a3ae99983f17
medved1ch <medved1ch@DESKTOP-I81IIK2>
2021-12-21 00:21:01 +0300

    release vers

--- Doctor/MainWindow.xaml.cs
+++ Doctor/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Windows;
 using Doctor.Connection;
 using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace Doctor
 {
@@ -30,12 +33,12 @@ namespace Doctor
                         byte[] Pass = f.GetHashPassword(PbPass.Password);
                         connection.Open();
                         string Number = TbPhone.Text;
-                        string query = $@"SELECT  COUNT(*) FROM Pacient WHERE Phone='{Number}' AND Password='{Pass}'";
+                        string query = $@"SELECT  COUNT(*) FROM Pacient WHERE Phone= '{Number}' AND Password= '{Pass}'";
                         SQLiteCommand cmd = new SQLiteCommand(query, connection);
                         int count = Convert.ToInt32(cmd.ExecuteScalar());
                         if (count == 1)
                         {
-                            string query2 = $@"SELECT id FROM Pacient WHERE Phone={Number}";
+                            string query2 = $@"SELECT id FROM Pacient WHERE Phone='{Number}'";
                             SQLiteCommand cmd2 = new SQLiteCommand(query2, connection);
                             int countID = Convert.ToInt32(cmd2.ExecuteScalar());
                             connection.Close();
@@ -64,6 +67,91 @@ namespace Doctor
             Registration reg = new Registration();
             reg.Owner = this;
             reg.ShowDialog();
+        }
+        private void PhoneMask(string Phone)
+        {
+            var newVal = Phone;
+            Phone = string.Empty;
+            switch (newVal.Length)
+            {
+                case 1:
+                    Phone = Regex.Replace(newVal, @"(\d{1})", "+7(___)___-__-__");
+                    break;
+                case 2:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{0,3})", "+7($2__)___-__-__");
+                    break;
+                case 3:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{0,3})", "+7($2_)___-__-__");
+                    break;
+                case 4:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{0,3})", "+7($2)___-__-__");
+                    break;
+                case 5:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})", "+7($2)$3__-__-__");
+                    break;
+                case 6:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})", "+7($2)$3_-__-__");
+                    break;
+                case 7:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})", "+7($2)$3-__-__");
+                    break;
+                case 8:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})(\d{0,2})", "+7($2)$3-$4_-__");
+                    break;
+                case 9:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})(\d{0,2})", "+7($2)$3-$4-__");
+                    break;
+                case 10:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})(\d{0,2})(\d{0,2})", "+7($2)$3-$4-$5_");
+                    break;
+                case 11:
+                    Phone = Regex.Replace(newVal, @"(\d{1})(\d{3})(\d{0,3})(\d{0,2})(\d{0,2})", "+7($2)$3-$4-$5");
+                    break;
+            }
+            TbPhone.Text = Phone;
+        }
+        private string replacenumber()
+        {
+            string num = Regex.Replace(TbPhone.Text, @"[^0-9]", "");
+            return num;
+        }
+        private void changeCaretIndex(string Phone)
+        {
+            if (Phone.Length <= 11)
+            {
+                PhoneMask(Phone);
+            }
+            if (Phone.Length <= 4)
+            {
+                TbPhone.CaretIndex = Phone.Length + 2;
+            }
+            else if (Phone.Length <= 7)
+            {
+                TbPhone.CaretIndex = Phone.Length + 3;
+            }
+            else if (Phone.Length <= 9)
+            {
+                TbPhone.CaretIndex = Phone.Length + 4;
+            }
+            else if (Phone.Length <= 11)
+            {
+                TbPhone.CaretIndex = Phone.Length + 5;
+            }
+        }
+        private void TbPhone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            changeCaretIndex(replacenumber());
+        }
+
+        private void TbPhone_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            changeCaretIndex(replacenumber() + e.Text);
+            e.Handled = true;
+        }
+
+        private void TbPhone_GotFocus(object sender, RoutedEventArgs e)
+        {
+            changeCaretIndex(replacenumber());
         }
     }
 }
